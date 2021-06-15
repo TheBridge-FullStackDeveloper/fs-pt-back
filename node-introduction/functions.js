7a701553c912ccf21fe97d4c61d39da47696bd97
TommyTraddles <jandresnova@gmail.com>
2021-06-15 23:22:22 +0200

    feat. 5, having some doubts, but still going

--- node-introduction/functions.js
+++ node-introduction/functions.js
@@ -1,4 +1,5 @@
-console.log('> jorge test')
+
+console.log('> starting ...')
 const fs = require('fs')
 const tap = a => console.log(a)
 
@@ -13,13 +14,21 @@ const read = path =>
 	fs.readFile(`./${path}`, (err, data) =>
 		err ? tap(err.message) : tap(data.toString() ))
 
+// 5
+// if true, write and read function wont work
+const itIsDir = path =>{
+	fs.stat(`./${path}`, (err, data) =>
+		err ? tap(err.message) : tap(data.isDirectory() ))
+}
+
 // 3 & 4
+// what if it is not write or read?
 const receiveDirByCmd = () => {
 	const path = process.argv[2]
 	const trigger = process.argv[3]
 	trigger === 'write' ? write(path) : read(path)
+	itIsDir(path)
 }
 receiveDirByCmd()
-
 
 
