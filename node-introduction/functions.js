318602419dccc7e17259fa277ad27e5c1835a3d2
TommyTraddles <jandresnova@gmail.com>
2021-06-16 13:09:13 +0200

    feat. 6 solved, but w/ bug when creating folder

--- node-introduction/functions.js
+++ node-introduction/functions.js
@@ -3,27 +3,33 @@
 const fs = require('fs')
 const tap = a => console.log(a)
 
-// 1 ✅  6 ❌
-// mkdir & writeFile ? 
+// 1  6 ❌
 const message = 'Not so rookie!'
-const write = path =>
-	fs.writeFile(`./${path}`, message, (err, data) =>
-		err ? tap(err.message) : tap(data) )
+const createDir = path =>{
+	const dir_name = path.slice(0 ,path.lastIndexOf('/') + 1)
+	fs.mkdir(`./${dir_name})`, {recursive: true}, (err, data) =>
+		err ? tap(err.message) : write(path, dir_name) )
+}
+const write = (path, dir_name) =>{
+	const file_name = path.slice(path.lastIndexOf('/') + 1, path.length)
+	fs.writeFile(`./${dir_name}${file_name}`, message, (err, data) =>
+		err ? tap(err.message) : tap(`> writefile: ${data}`) )
+}
 
-// 2 ✅
+// 2
 const read = path =>
 	fs.readFile(`./${path}`, (err, data) =>
-		err ? tap(err.message) : tap(data.toString() ))
+		err ? tap(err.message) : tap(`> read: ${data.toString()}` ))
 
-// 5 ✅
+// 5
 const directory = path =>
 	fs.stat(`./${path}`, (err, data) =>
-		err ? tap(err.message) : tap(data.isDirectory() ))
+		err ? tap(err.message) : tap(`> is directory?: ${data.isDirectory()}`))
 
-// 7 ✅
+// 7
 const size = path =>
 	fs.stat(`./${path}`, (err, data) =>
-		err ? tap(err.message) : console.log(data.size, 'bytes') )
+		err ? tap(err.message) : console.log(`> size:  ${data.size} bytes`) )
 
 // 8 ❌
 const copy = path =>
@@ -37,13 +43,12 @@ const list = path =>
 const deepList = path =>
 	tap('> deepList')
 
-// 3 ✅ 4 ✅
-// what if it is not write or read?
+// 3 4
 const receiveDirByCmd = () => {
 	const path = process.argv[2]
 	const trigger = process.argv[3]
 	if (trigger === 'write'){
-		write(path)
+		createDir(path)
 	} if (trigger === 'read') {
 		read(path)
 	} if (trigger === 'copy'){
