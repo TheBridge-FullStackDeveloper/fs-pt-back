045eb320ac794463e0ca8aaf900072b4e0afde7d
maidersonn <maidersonn@gmail.com>
2021-06-27 02:03:52 +0200

    fixed exercise of user-songs

--- exercise-mongoose-songs/routes/userRoutes/index.js
+++ exercise-mongoose-songs/routes/userRoutes/index.js
@@ -1,12 +1,28 @@
 const router = require("express").Router();
 const UserModel = require("../../models/User");
+const SongModel = require("../../models/Songs");
 
-router.post("/add-song-to-playlist", async (req, res) => {
+router.post("/create-user", async (req, res) => {
   try {
     const newUser = req.body;
     const result = await UserModel.create(newUser);
     res.status(200).json({ result });
-  } catch {
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+router.put("/users/:_id/add-song/:songid", async (req, res) => {
+  try {
+    const { _id, songid } = req.params;
+    const user = await UserModel.findOne({ _id });
+    user.playlist.push(songid);
+
+    const result = await UserModel.findOneAndUpdate({ _id }, user, {
+      new: true,
+    });
+    res.status(200).json({ result });
+  } catch (err) {
     res.status(500).json({ err });
   }
 });
@@ -15,32 +31,32 @@ router.get("/get-user-playlist", async (req, res) => {
   try {
     const result = await UserModel.find({}).populate("playlist");
     res.status(200).json({ result });
-  } catch {
+  } catch (err) {
     res.status(500).json({ err });
   }
 });
 
-router.delete("/delete-song-of-playlist/:_id", async (req, res) => {
+router.delete("/delete-user/:_id", async (req, res) => {
   try {
     const { _id } = req.params;
     const result = await UserModel.findOneAndDelete({ _id });
     res.status(200).json({ result });
-  } catch {
+  } catch (err) {
     res.status(500).json({ err });
   }
 });
 
-router.put("/update-user-playlist/:_id", async (req, res) => {
+router.put("/update-user/:_id", async (req, res) => {
   try {
-    const modifiedPlaylist = req.body;
+    const modifiedUser = req.body;
     const { _id } = req.params;
 
-    const result = await UserModel.findOneAndUpdate({ _id }, modifiedPlaylist, {
+    const result = await UserModel.findOneAndUpdate({ _id }, modifiedUser, {
       new: true,
     });
 
     res.status(200).json({ result });
-  } catch {
+  } catch (err) {
     res.status(500).json({ err });
   }
 });
